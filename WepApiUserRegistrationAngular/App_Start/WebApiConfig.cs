30c5f8e601a394c6865a36507b5e35281d69275d
sampanna pokhrel <sampanna99@gmail.com>
2018-03-26 22:02:13 -0400

    added cors

--- WepApiUserRegistrationAngular/App_Start/WebApiConfig.cs
+++ WepApiUserRegistrationAngular/App_Start/WebApiConfig.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
+﻿using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace WepApiUserRegistrationAngular
 {
@@ -9,6 +7,8 @@ namespace WepApiUserRegistrationAngular
     {
         public static void Register(HttpConfiguration config)
         {
+            config.EnableCors(new EnableCorsAttribute("http://localhost:4200/", headers: "*", methods: "*"));
+
             // Web API configuration and services
 
             // Web API routes
--- WepApiUserRegistrationAngular/Controllers/AccountController.cs
+++ WepApiUserRegistrationAngular/Controllers/AccountController.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System.Web.Http;
-using Microsoft.AspNet.Identity;
+using WepApiUserRegistrationAngular.Models;
 
 namespace WepApiUserRegistrationAngular.Controllers
 {
@@ -12,9 +9,16 @@ namespace WepApiUserRegistrationAngular.Controllers
     {
         [Route("api/User/Register")]
         [HttpPost]
-        public IdentityResult Register()
+        public IdentityResult Register(AccountModel model)
         {
-            
+            var userStore = new UserStore<ApplicationUser>(new ApplicationDbContext());
+            var manager = new UserManager<ApplicationUser>(userStore);
+            var user = new ApplicationUser { UserName = model.UserName, Email = model.Email };
+            user.FirstName = model.FirstName;
+            user.LastName = model.LastName;
+            manager.PasswordValidator = new PasswordValidator { RequiredLength = 3 };
+            IdentityResult result = manager.Create(user, model.Password);
+            return result;
         }
     }
 }
